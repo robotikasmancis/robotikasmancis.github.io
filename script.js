6a90acddfcb843ebea2ec51aec1520dc37364eb6
robotikasmancis <farandsastrawinata@gmail.com>
2025-07-16 22:53:53 +0700

    Menambahkan link pada tombol daftar

---
+++ script.js
@@ -0,0 +1,5 @@
+let tombol = document.getElementsByClassName("tombol");
+
+tombol[0].addEventListener("click", function() {
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSfOuXP20LEGyVP3Z_l0HD8K_WG6XtbepiTnQUCqaFZ-ger9Og/viewform?usp=sharing&ouid=100182362586205452496", "_blank");
+});
